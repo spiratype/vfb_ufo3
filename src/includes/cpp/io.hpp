98545a947578d1b413a3354f0f67d7847fb18e2e
spiratype <44244812+spiratype@users.noreply.github.com>
2020-07-13 21:29:00 -0500

    Delete io.hpp

--- src/includes/cpp/io.hpp
+++
@@ -1,7 +0,0 @@
-// io.hpp
-
-void write_file(const char* path, const char* text, const size_t text_len) {
-	std::FILE* f = std::fopen(path, "w");
-	std::fwrite(text, 1, text_len, f);
-	std::fclose(f);
-	}
